0776e10028c5e179940359e4c1a24a4b664b9cb0
Sofia Barreiro <sofiabarreiro@cloud.upwork.com>
2022-06-29 12:04:02 -0300

    close navbar when navigating

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { LocaleSelector } from './LocaleSelector';
 import { FormattedMessage } from 'react-intl';
 
-export const NavBar = ({selectedLocale, onChangeLocale}) => {
+const navBarElements = [
+  { href: "#experience", title: 'experience' },
+  { href: "#education", title: 'education' },
+  { href: "#languages", title: 'languages' },
+  { href: "#contact", title: 'contact' }
+]
+
+export const NavBar = ({ selectedLocale, onChangeLocale }) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top" expanded={expanded}>
       <Container>
         <Navbar.Brand href="#">Sofía Barreiro</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Toggle aria-controls="navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
         <Navbar.Collapse id="navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#experience"><FormattedMessage id="experience"/></Nav.Link>
-            <Nav.Link href="#education"><FormattedMessage id="education"/></Nav.Link>
-            <Nav.Link href="#languages"><FormattedMessage id="languages"/></Nav.Link>
-            <Nav.Link href="#contact"><FormattedMessage id="contact"/></Nav.Link>
+            {navBarElements.map(item => (
+              <Nav.Link href={item.href} onClick={() => setExpanded(false)}><FormattedMessage id={item.title} /></Nav.Link>
+            ))}
           </Nav>
           <Nav>
             <LocaleSelector selectedLocale={selectedLocale} onChangeLocale={onChangeLocale} />
